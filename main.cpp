f067c8dd19426dc13199d782c3593c5e458a2a38
jfl747076@gmail.com <jfl747076@gmail.com>
2019-04-02 22:49:24 -0500

    Remove %u and other such nonsense from the exec. We're a launcher, not a file manager

--- main.cpp
+++ main.cpp
@@ -47,6 +47,14 @@ inline bool tryHandleKey(std::unordered_map<SDL_Keycode, std::function<bool()>>&
 
 using AppList = std::unordered_map<std::string, std::string>;
 
+bool replace(std::string& str, const std::string& from, const std::string& to) {
+    size_t start_pos = str.find(from);
+    if(start_pos == std::string::npos)
+        return false;
+    str.replace(start_pos, from.length(), to);
+    return true;
+}
+
 void addApps(AppList& apps, std::string path) {
     for (auto& f : std::filesystem::directory_iterator(path)) {
         if (!f.is_regular_file()) continue;
@@ -57,7 +65,16 @@ void addApps(AppList& apps, std::string path) {
 
         auto& entry = ini["Desktop Entry"];
         auto& name  = entry["Name"];
-        auto& exec  = entry["Exec"];
+        auto exec  = entry["Exec"];
+        replace(exec, "%f", "");
+        replace(exec, "%F", "");
+        replace(exec, "%d", "");
+        replace(exec, "%D", "");
+        replace(exec, "%u", "");
+        replace(exec, "%U", "");
+        replace(exec, "%N", "");
+        replace(exec, "%k", "");
+        replace(exec, "%v", "");
 
         if (name.empty() || exec.empty()) continue;
 
