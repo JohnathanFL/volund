84743982ae555b577a8471021de2be14a4558356
jfl747076@gmail.com <jfl747076@gmail.com>
2019-04-03 12:13:00 -0500

    Add handler for enter to select first item

--- main.cpp
+++ main.cpp
@@ -49,8 +49,7 @@ using AppList = std::unordered_map<std::string, std::string>;
 
 bool replace(std::string& str, const std::string& from, const std::string& to) {
     size_t start_pos = str.find(from);
-    if(start_pos == std::string::npos)
-        return false;
+    if (start_pos == std::string::npos) return false;
     str.replace(start_pos, from.length(), to);
     return true;
 }
@@ -65,7 +64,7 @@ void addApps(AppList& apps, std::string path) {
 
         auto& entry = ini["Desktop Entry"];
         auto& name  = entry["Name"];
-        auto exec  = entry["Exec"];
+        auto exec   = entry["Exec"];
         replace(exec, "%f", "");
         replace(exec, "%F", "");
         replace(exec, "%d", "");
@@ -105,12 +104,12 @@ int main() {
 
     while (running) {
         if (!shown) {
-            continue;
+            std::this_thread::sleep_for(snappiness);
         } else {
             Vec2<int> windowSize;
-            auto window = SDL_CreateWindow("volund", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1920, 1080,
+            auto window = SDL_CreateWindow("volund", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 200,
                                            SDL_WINDOW_OPENGL | SDL_WINDOW_BORDERLESS | SDL_WINDOW_SHOWN |
-                                               SDL_WINDOW_INPUT_GRABBED | SDL_WINDOW_FULLSCREEN);
+                                               SDL_WINDOW_INPUT_GRABBED);
 
             SDL_GetWindowSize(window, &windowSize.x, &windowSize.y);
             SDL_GLContext glCtx = SDL_GL_CreateContext(window);
@@ -138,6 +137,13 @@ int main() {
                                 return true;
                             }});
 
+            keyMaps.insert({SDLK_RETURN, [&]() {
+                                searchText[0] = '\0';
+                                if (topTen[0]) system(("setsid " + apps[*topTen[0]] + '&').c_str());
+                                shown = false;
+                                return true;
+                            }});
+
             while (running && shown) {
                 if (searchText[0] == '\0' && searchText != prevText) {
                     topTen.clear();
@@ -147,13 +153,12 @@ int main() {
 
                 else if (searchText[0] != '\0' && searchText != prevText) {
 
-
-                    std::cerr << "Checking against '";
+                    // std::cerr << "Checking against '";
                     auto txt = searchText;
                     txt.erase(std::find(txt.begin(), txt.end(), '\0'), txt.end());
 
                     auto exp = "(.*)" + txt + "(.*)";
-                    std::cerr << "Updating top apps with expression '" << exp << "'\n";
+                    // std::cerr << "Updating top apps with expression '" << exp << "'\n";
                     auto regex = std::regex(exp, std::regex_constants::icase);
 
                     std::cerr << "'\n";
@@ -161,10 +166,9 @@ int main() {
                     topTen.clear();
                     for (const auto& app : apps) {
 
-
-
                         if (std::regex_match(app.first, regex)) {
-                          topTen.push_back(&app.first); }
+                            topTen.push_back(&app.first);
+                        }
                     }
 
                     prevText = searchText;
@@ -185,11 +189,13 @@ int main() {
 
                 if (nk_begin(ctx, "volund", nk_rect(0, 0, windowSize.x, windowSize.y), 0)) {
                     nk_layout_row_dynamic(ctx, 25, 1);
+                    nk_edit_focus(ctx, NK_EDIT_ALWAYS_INSERT_MODE);
                     nk_edit_string_zero_terminated(ctx, NK_EDIT_FIELD, &searchText[0], 127, searchFilter);
 
                     for (size_t i = 0; i < topTen.size() && i < 9; i++) {
                         nk_layout_row_dynamic(ctx, 25.0, 1);
-                        if (nk_button_label(ctx, topTen[i]->c_str())) {
+                        if (nk_button_label(ctx, (i == 0 ? (std::string{">>  "} + *topTen[i] + "  <<").c_str()
+                                                         : topTen[i]->c_str()))) {
                             system(("setsid " + apps[*topTen[i]] + '&').c_str());
                             shown = false;
                             goto cleanupLoopIter; // Our Bjorne who art in heaven above forgive me
@@ -213,8 +219,6 @@ int main() {
             SDL_GL_DeleteContext(glCtx);
             SDL_DestroyWindow(window);
         }
-
-        std::this_thread::sleep_for(snappiness);
     }
 
     SDL_Quit();
